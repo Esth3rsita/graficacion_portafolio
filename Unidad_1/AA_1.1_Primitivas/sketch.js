9d66640824e31d05f99c03e44ce7d222a93737f1
BLANCA ESTHER GONZALEZ RAMIREZ <251k0025@itscarrillopuerto.edu.mx>
2026-08-29 18:39:35 -0500

    Update sketch.js

--- Unidad_1/AA_1.1_Primitivas/sketch.js
+++ Unidad_1/AA_1.1_Primitivas/sketch.js
@@ -1,1 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+}
 
+function draw() {
+  background(255, 192, 203); 
+
+  // Antenitas
+  strokeWeight(10); 
+  line(150, 100, 150, 50); 
+  line(250, 100, 250, 50); 
+
+  // Cara y Boca 
+  strokeWeight(2); 
+  
+  stroke(0, 0, 0);
+  fill(128, 128, 128);      
+  rect(100, 100, 200, 200); 
+  
+  fill(255, 0, 0);          
+  rect(150, 240, 100, 30);  
+
+  // Ojos 
+  fill(255, 255, 255);       
+  ellipse(150, 160, 40, 40); 
+  ellipse(250, 160, 40, 40); 
+
+  // Nariz 
+  fill(255, 165, 0);        
+  triangle(200, 180, 170, 220, 230, 220); 
+}
